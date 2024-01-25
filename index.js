e423dcf0826fa27e5ec0919a2242e9aea8e75be7
DillonBorden <dillondaughtry2012@gmail.com>
2024-01-24 18:09:51 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, string){
+    const lowerCaseString = string.toLowerCase();
+    
+    const matchingDrivers = drivers.filter(driver => {
+        const lowerCaseDriver = driver.toLowerCase();
+        return lowerCaseDriver === lowerCaseString;
+    });
+    return matchingDrivers;
+}
+
+
+function fuzzyMatch(drivers, string){
+    const matchingDrivers = drivers.filter(obj => obj.startsWith(string) );
+
+    return matchingDrivers;
+}
+
+function matchName(driver, string){
+    const {name, hometown} = driver;
+
+    const matchingDrivers = driver.filter(obj => obj.name === string);
+
+    return matchingDrivers;
+}
